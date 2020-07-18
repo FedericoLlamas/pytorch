b5bed90cb414ee0fab9e343c9917246b80bfdacd
Federico Llamas <federico@comparaencasa.com>
2020-07-18 18:16:56 -0300

    Cargo un nuevo test, classifier. Una red neuronal para clasificar imágenes en 10 clases. Utilizando CIFAR-10

---
+++ classifier.py
@@ -0,0 +1,168 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+# functions to show an image
+# def imshow(img):
+#     img = img / 2 + 0.5     # unnormalize
+#     npimg = img.numpy()
+#     plt.imshow(np.transpose(npimg, (1, 2, 0)))
+#     plt.show()
+
+
+def start_classifier():
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                              shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                             shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    # get some random training images
+    # dataiter = iter(trainloader)
+    # images, labels = dataiter.next()
+
+    # show images
+    # imshow(torchvision.utils.make_grid(images))
+    # print labels
+    # print('Loader training labels')
+    # print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+    return classes, trainset, trainloader, testset, testloader
+
+
+def training(starting_data):
+    # Set to use device GPU
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    classes, trainset, trainloader, testset, testloader = starting_data
+    net = Net()
+    # Uso GPU cuda:0
+    net.to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    for epoch in range(2):  # loop over the dataset multiple times
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            # port cada for, son 4 imagenes con sus labels
+            # uso GPU cuda:0
+            inputs, labels = data[0].to(device), data[1].to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+    print('##Finished Training##')
+    PATH = './cifar_net.pth'
+    torch.save(net.state_dict(), PATH)
+    print('##Training saved##')
+
+
+def testing(starting_data):
+    print('## start testing ##')
+    PATH = './cifar_net.pth'
+    classes, trainset, trainloader, testset, testloader = starting_data
+    dataiter = iter(testloader)
+    images, labels = dataiter.next()
+
+    # print images
+    # imshow(torchvision.utils.make_grid(images))
+    print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+    # Reload saved model
+    net = Net()
+    net.load_state_dict(torch.load(PATH))
+    outputs = net(images)
+    '''
+    El torch.max de cada vector obtenido de la red para las 4 imagenes,
+    nos devuelve el valor máximo y el índice correspondiente
+    (que equicale a la clase a la que hay mayor posibilidad de
+    que corresponda.)
+    '''
+    _, predicted = torch.max(outputs, 1)
+
+    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
+                                  for j in range(4)))
+
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs, 1)
+            c = (predicted == labels).squeeze()
+            for i in range(4):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
+
+    for i in range(10):
+        print('Accuracy of %5s : %2d %%' % (
+            classes[i], 100 * class_correct[i] / class_total[i]))
+    print('## finished testing ##')
+
+
+'''
+START CODE
+'''
+
+
+starting_data = start_classifier()
+'''
+Entrnamiento de la red
+'''
+# training(starting_data)
+
+'''
+Testeo del entrenamiento
+'''
+testing(starting_data)
+
